e8fe0872711133a1c8cc3e2a7779780666005fe7
Eguelin <eguelin@student.42lyon.fr>
2024-02-25 16:36:45 +0100

    little fix

--- ex00/include/whatever.hpp
+++ ex00/include/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 17:50:00 by eguelin           #+#    #+#             */
-/*   Updated: 2023/12/09 15:16:47 by eguelin          ###   ########lyon.fr   */
+/*   Updated: 2024/02/13 14:55:17 by eguelin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,13 @@ void	swap( T &a, T &b)
 template <typename T>
 const T	&min( T &a, T &b)
 {
-	return ( a <= b ? a : b);
+	return ( a < b ? a : b);
 }
 
 template <typename T>
 const T	&max( T &a, T &b)
 {
-	return ( a >= b ? a : b);
+	return ( a > b ? a : b);
 }
 
 #endif
